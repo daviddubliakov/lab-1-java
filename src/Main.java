85ce7ef73a5846949feac01f797da58f3f9fdfca
daviddubliakov <dubliakov1@gmail.com>
2024-04-20 14:02:49 +0300

    adds text updates

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import java.util.Random;
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -12,6 +13,8 @@ public class Main {
             int stepsToAdd = random.nextInt(10 - 1 + 1) + 10; // Generates random number from 1 to 10
             new MainThread(threadId,stepsToAdd, breakThread).start();
         }
+
+        System.out.println("Started amount of threads: " + numberOfThreads);
 
         new Thread(breakThread).start();
 
